e7ce683f036a393948519a058464ba18327934e3
julgargal <juliagarciagallego@hotmail.com>
2019-01-31 09:24:50 +0100

    Correción del error #1

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -15,18 +15,18 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-#from django.urls import path, include
+from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
 
 schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
-#    path('admin/', admin.site.urls),
-#    path('doc/', schema_view)
+    path('admin/', admin.site.urls),
+    path('doc/', schema_view)
 ]
 
 for module in settings.MODULES:
     urlpatterns += [
-#        path('{}/'.format(module), include('{}.urls'.format(module)))
+       path('{}/'.format(module), include('{}.urls'.format(module)))
     ]
